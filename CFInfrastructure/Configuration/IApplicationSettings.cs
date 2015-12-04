de79ff0041e06add4c54d8bdc8c4f0ff46e71272
hy <cengling17@hotmail.com>
2015-12-04 15:09:06 +0800

    2015/12/04

---
+++ CFInfrastructure/Configuration/IApplicationSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace CFInfrastructure.Configuration
+{
+    public class ApplicationSettings
+    {
+        public static string GetPropertyValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
