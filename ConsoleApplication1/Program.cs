e40a33c051ac893bacef82562dc97fabcca8dbf5
hy <cengling17@hotmail.com>
2016-06-23 17:31:24 +0800

    20160623

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using DotNet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = ConvertJson.ToJson(new TT { Name = "hy", Sex = "Female" });
+            Console.WriteLine(s);
+        }
+    }
+
+    public class TT
+    {
+        public String Name { get; set; }
+        public String Sex { get; set; }
+    }
+}
